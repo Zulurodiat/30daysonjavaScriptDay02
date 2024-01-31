b3e71cea520f3fb9637179c93f78b9274ccb4c96
zulurodiat <zulurodiat@gmail.com>
2024-01-31 11:23:28 +0100

    first commit

---
+++ variable.js
@@ -0,0 +1,29 @@
+let challenge= '30 Days of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(0,2))
+console.log(challenge.substring(2,21))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(''))
+console.log(Array('Facebook, Googele, Microsoft, IBM, Oracle, Amazon'))
+console.log(challenge.replace("JavaScript", "Python"))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt(11))
+console.log(challenge.indexOf('a'))
+console.log(challenge.lastIndexOf('a'))
+let words= 'You cannot end a sentence with because because because is a conjunction'
+console.log(words.indexOf('because'))
+console.log(words.lastIndexOf('because'))
+console.log(words.search('because'))
+console.log(challenge.trim())
+console.log(challenge.startsWith('30'))
+console.log(challenge.endsWith('JavaScript'))
+console.log(challenge.match('a'))
+let java= 'JavaScript'
+console.log(java.concat('30 Days of JavaScript'))
+console.log(challenge.repeat(2))
+
+
